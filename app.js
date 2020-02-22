9c6f8794fd2ac4aa2dfe119c2b57e7ef3c7b5b4e
Nitin Birdi <nitin.birdi@icloud.com>
2020-02-23 01:43:16 +0530

    new commit

--- app.js
+++ app.js
@@ -5,7 +5,15 @@ var request = require("request");
 
 var app = express();
 
-app.use({})
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+// app.post("/", function(req, res) {
+// });
 
 
 app.listen(3000, function() {
