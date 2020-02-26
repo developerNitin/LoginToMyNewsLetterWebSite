b6955940d14a5e46312127cb09c124ad1c159282
Nitin Birdi <nitin.birdi@icloud.com>
2020-02-26 23:05:03 +0530

    heroku heroku

--- app.js
+++ app.js
@@ -63,6 +63,6 @@ app.post("/failure", function(req, res) {
 });
 
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("server is running on port 3000");
 });
