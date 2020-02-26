7db8e8c19b335ef95a3a658ee263d88409e686f0
Nitin Birdi <nitin.birdi@icloud.com>
2020-02-26 14:13:28 +0530

    another one

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-//jshint esversion: 6
+// jshint esversion: 6
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const request = require("request");
+// const request = require("request");
 
 const app = express();
 
@@ -15,7 +15,6 @@ app.get("/", function(req, res) {
 
 app.post("/", function(req, res) {
 
-  const apiKey = "215692c5888592d4583b79a21de2714d-us4";
   const firstname = req.body.fname;
   const lastname = req.body.lname;
   const emailID = req.body.email;
@@ -34,9 +33,21 @@ app.post("/", function(req, res) {
   };
 });
 
+const jsonData = JSON.stringify(data);
+const url = "https://us4.api.mailchimp.com/3.0/lists/d0e7323970";
+const option = {
+  method: "POST",
+  auth: "Nitin:215692c5888592d4583b79a21de2714d-us4"
+};
+
+const request = https.request(url, option, function(response) {
+  response.on("data", function(data) {
+    console.log(JSON.parse(data));
+  });
+});
+
+
 
 app.listen(3000, function() {
   console.log("server is running on port 3000");
 });
-
-//d0e7323970
